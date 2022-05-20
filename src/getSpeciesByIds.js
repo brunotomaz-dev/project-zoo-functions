aa43632fd694b1ec50eb8398433fe1b4856318fa
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-05-20 17:23:30 -0300

    Requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  const find = [];
+  ids.forEach((animal) => {
+    find.push(species.find(({ id }) => id === animal));
+  });
+  return find;
 }
 
 module.exports = getSpeciesByIds;
