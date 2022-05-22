a34b0f12be06f506516a90536b3db9e048e9e553
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-05-22 14:59:44 -0300

    Requisito 4 ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    return employees.filter(({ managers }) => managers.includes(managerId))
+      .map((employeesNames) => `${employeesNames.firstName} ${employeesNames.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
