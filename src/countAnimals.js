cbc45ec584ff3e31953dae508d8b8c49022c48d1
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-05-22 21:34:40 -0300

    Requisito 5 ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+
+  if (animal === undefined) {
+    const animals = {};
+    species.forEach((animalSpecie) => {
+      animals[animalSpecie.name] = animalSpecie.residents.length;
+    });
+    return animals;
+  }
+
+  const { specie } = animal;
+
+  if (animal.sex === undefined) {
+    return species.find(({ name }) => name === specie).residents.length;
+  }
+  const { sex: animalSex } = animal;
+  return species.find(({ name }) => name === specie)
+    .residents.filter(({ sex }) => sex === animalSex).length;
 }
 
 module.exports = countAnimals;
