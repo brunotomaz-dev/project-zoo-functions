d13b14df0586f82115c300b4beb5b8f4a44e7879
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-05-23 23:13:59 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return undefined;
+  }
+  const children = entrants.filter(({ age }) => age < 18).length;
+  const adults = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const seniors = entrants.filter(({ age }) => age >= 50).length;
+  return { child: children, adult: adults, senior: seniors };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length < 1) {
+    return 0;
+  }
+  const calculateEntrants = countEntrants(entrants);
+  const key = Object.keys(calculateEntrants);
+  return key.map((objectKey) => calculateEntrants[objectKey] * prices[objectKey])
+    .reduce((acc, curr) => acc + curr);
 }
 
 module.exports = { calculateEntry, countEntrants };
