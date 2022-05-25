319506c4d6c21b4f8d14eeb4fdbab1de41ad8b8b
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-05-25 20:54:52 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findEmployee = employees.find((employee) => employee.id === id);
+  const idSpecie = findEmployee.responsibleFor[0];
+  const specieAnimals = species.find((animal) => animal.id === idSpecie).residents;
+  const findOlder = specieAnimals.map(({ age }) => age).sort((a, b) => b - a);
+  const speciesOlder = specieAnimals.find((animal) => animal.age === findOlder[0]);
+  return [speciesOlder.name, speciesOlder.sex, speciesOlder.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
